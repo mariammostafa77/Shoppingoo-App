f48297fd2f87c54b032cde72f14815ed6881551e
Zeinab Ibrahim <72860288+zeinab414@users.noreply.github.com>
2022-05-31 00:25:46 +0200

    nav graph last version

--- app/src/main/java/com/example/mcommerce/HomeActivity.kt
+++ app/src/main/java/com/example/mcommerce/HomeActivity.kt
@@ -3,6 +3,9 @@ package com.example.mcommerce
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.fragment.NavHostFragment
 import com.example.mcommerce.home.view.HomeFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -18,7 +21,17 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         bottomNavigationView = findViewById(R.id.buttomNav)
-        replaceFragment(homeFragment)
+       // replaceFragment(homeFragment)
+
+        //getSupportFragmentManager().beginTransaction().replace(R.id.viewLayout,new HomeFragment()).commit();
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.frameLayout) as NavHostFragment?
+        val navController = navHostFragment!!.navController
+        val navGraph = navHostFragment!!.navController.navInflater.inflate(R.navigation.my_nav_graph)
+        // navGraph.setStartDestination(R.id.fragmentAddMed1);
+        // navGraph.setStartDestination(R.id.fragmentAddMed1);
+        navGraph.setStartDestination(R.id.homeFragment)
+        navController.graph = navGraph
 
         bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
