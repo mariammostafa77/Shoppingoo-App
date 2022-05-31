0474229d7302b654cfcb12f79d9be0226b52c988
Asmaa Youssef <asmaayoussef786@gmail.com>
2022-05-31 18:17:49 +0200

    Add Ads design and implementation.

--- app/src/main/java/com/example/mcommerce/home/view/HomeFragment.kt
+++ app/src/main/java/com/example/mcommerce/home/view/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.example.mcommerce.home.view
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,6 +12,9 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.coroutineScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
 import com.example.mcommerce.R
 import com.example.mcommerce.network.RetrofitHelper
 import com.example.mcommerce.network.ServiceApi
@@ -18,6 +23,7 @@ import com.example.mcommerce.home.viewModel.HomeViewModelFactory
 import com.example.mcommerce.model.Repository
 import com.example.mcommerce.network.BrandsClient
 import kotlinx.coroutines.*
+import kotlin.math.abs
 
 
 class HomeFragment : Fragment() {
@@ -28,13 +34,30 @@ class HomeFragment : Fragment() {
     lateinit var homeViewModel: HomeViewModel
     lateinit var linearLayoutManager: LinearLayoutManager
 
+    private lateinit var  adsViewPager: ViewPager2
+    private lateinit var handler : Handler
+    private lateinit var adsImageList:ArrayList<Int>
+    private lateinit var adsAdapter: AdsAdapter
 
+    private val runnable = Runnable {
+        adsViewPager.currentItem = adsViewPager.currentItem + 1
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         var view:View = inflater.inflate(R.layout.fragment_home, container, false)
+
+        initAdsViewPager(view)
+        setUpTransformer()
+        adsViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                handler.removeCallbacks(runnable)
+                handler.postDelayed(runnable , 2000)
+            }
+        })
 
         bradsRecyclerView=view.findViewById(R.id.bradsRecyclerView)
         linearLayoutManager=LinearLayoutManager(requireContext())
@@ -57,6 +80,39 @@ class HomeFragment : Fragment() {
 
 
         return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(runnable , 2000)
+    }
+    private fun initAdsViewPager(view : View){
+        adsViewPager = view.findViewById(R.id.adsViewPager)
+        handler = Handler(Looper.myLooper()!!)
+        adsImageList = ArrayList()
+        adsImageList.add(R.drawable.ads0)
+        adsImageList.add(R.drawable.ads1)
+        adsImageList.add(R.drawable.ads5)
+        adsImageList.add(R.drawable.ads3)
+        adsImageList.add(R.drawable.ads4)
+        adsImageList.add(R.drawable.ads6)
+        adsImageList.add(R.drawable.ads2)
+        adsAdapter = AdsAdapter(adsImageList, adsViewPager)
+        adsViewPager.adapter = adsAdapter
+        adsViewPager.offscreenPageLimit = 3
+        adsViewPager.clipToPadding = false
+        adsViewPager.clipChildren = false
+        adsViewPager.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+    }
+
+    private fun setUpTransformer(){
+        val transformer = CompositePageTransformer()
+        transformer.addTransformer(MarginPageTransformer(40))
+        transformer.addTransformer { page, position ->
+            val r = 1 - abs(position)
+            page.scaleY = 0.85f + r * 0.14f
+        }
+        adsViewPager.setPageTransformer(transformer)
     }
 
 
