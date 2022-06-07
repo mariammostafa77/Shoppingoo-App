f3007164b4bb82bb0ed52df4b2a238c8640bbcf6
mariammostafa77 <mariammostafa7799@gmail.com>
2022-06-07 16:13:31 +0200

    integrate categories with shipping card

--- app/src/main/java/com/example/mcommerce/network/RemoteSourceInterface.kt
+++ app/src/main/java/com/example/mcommerce/network/RemoteSourceInterface.kt
@@ -9,7 +9,14 @@ import com.example.mcommerce.draftModel.DraftOrderX
 import com.example.mcommerce.draftModel.DraftResponse
 import com.example.mcommerce.home.model.BrandsModel
 import com.example.mcommerce.model.*
+import com.example.mcommerce.model.AllProductsModel
+import com.example.mcommerce.model.ProductDetails
+import com.example.mcommerce.model.DiscountCodesModel
+import com.example.mcommerce.model.*
 import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.Path
+import retrofit2.http.Field
 
 
 interface RemoteSourceInterface {
@@ -19,14 +26,18 @@ interface RemoteSourceInterface {
     suspend fun getSpecificProduct(id:String): ProductDetails
     suspend fun getVariant(id:String): Variants
     suspend fun getSubCategories(vendor: String,productType:String,collectionId:String):AllProductsModel
+
     ///// Coupons
     suspend fun getDiscountCodes() : DiscountCodesModel
     suspend fun postNewCustomer(customer: CustomerDetail):Response<CustomerDetail>
+
     suspend fun getUserDetails(id:String): CustomerDetail
+
     suspend fun addNewAddress(id: String?, customer: CustomerDetail): Response<CustomerDetail>
     suspend fun changeCustomerCurrency(id: String? , customer: CustomerDetail): Response<CustomerDetail>
 
     suspend fun postNewDraftOrder(order: DraftOrder):Response<DraftOrder>
     suspend fun getCustomers(): cust_details
+
     suspend fun getShoppingCartProducts(): DraftResponse
 }
