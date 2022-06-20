4bacafbdfbd1e6d2516434a29efc3374b401b8b0
mariammostafa77 <mariammostafa7799@gmail.com>
2022-06-20 18:40:26 +0200

    add test dependencies

---
+++ app/src/test/java/com/example/mcommerce/orders/view/OrdersFragmentTest.kt
@@ -0,0 +1,13 @@
+package com.example.mcommerce.orders.view
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import junit.framework.TestCase
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class OrdersFragmentTest : TestCase(){
+
+
+
+}
